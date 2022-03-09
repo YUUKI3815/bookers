2645b244b2f8d9d7b4ca5e9c399915574e0daaab
EC2 Default User <ec2-user@ip-172-31-7-175.ec2.internal>
2022-03-09 09:19:38 +0000

    [add]モデルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_063236) do
+ActiveRecord::Schema.define(version: 2022_03_09_091536) do
 
-  create_table "lists", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
